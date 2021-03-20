d53c3627b048f4fc8e8f4f57b3c96bb5cc84f7a0
89tamas.fekete@gmail.com <89tamas.fekete@gmail.com>
2021-03-20 15:05:02 +0100

    added new test case

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -2,3 +2,6 @@ from helper import helper as hl
 
 def test1_stringToCoordinate():
     assert hl.stringToCoordinate("b3") == 12
+
+def test2_stringToCoordinate():
+    assert hl.stringToCoordinate("a1") == 0
